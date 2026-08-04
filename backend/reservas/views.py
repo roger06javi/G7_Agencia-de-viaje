11c18b41780393b27b6dd429dd70c96e0fc2bac9
eliantulmo4119 <elian.tulmo4119@utc.edu.ec>
2026-08-04 09:57:14 -0500

    Modifcamos el punto jsx de clientes por errores de logica

--- backend/reservas/views.py
+++ backend/reservas/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Reserva
 from .serializers import ReservaSerializer
-# Creamos la vista de reserva asa
+# Creamos la vista de reserva asa asd
 class ReservaViewSet(viewsets.ModelViewSet):
     queryset = Reserva.objects.all().order_by('-id')
     serializer_class = ReservaSerializer
