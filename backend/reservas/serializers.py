019708d26580da7e347feb154827bc5b700e40d9
eliantulmo4119 <elian.tulmo4119@utc.edu.ec>
2026-08-04 09:52:42 -0500

    Creamos el serializaer para el modelo de reserva y sus URLS

--- backend/reservas/serializers.py
+++ backend/reservas/serializers.py
@@ -1,7 +1,7 @@
 # Crear serializer para el modelo de reserva, con campos de fecha, cantidad de personas, estado, cliente y paquete turístico.
 from rest_framework import serializers
 from .models import Reserva
-
+# Creamos el serializer para el modelo de reserva
 class ReservaSerializer(serializers.ModelSerializer):
     nombre_cliente = serializers.ReadOnlyField(source='cliente.nombre')
     apellido_cliente = serializers.ReadOnlyField(source='cliente.apellido')
