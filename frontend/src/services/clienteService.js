6f968192bf941eed0d0caa7e1a807a8392cc5478
Camila Tuso <camila.tuso9119@utc.edu.ec>
2026-08-03 21:58:23 -0500

    Agregar comentarios al servicio de clientes

--- frontend/src/services/clienteService.js
+++ frontend/src/services/clienteService.js
@@ -1,17 +1,24 @@
 import api from './api';
 
+// Obtener todos los clientes
 export const obtenerClientes = async () => {
     const res = await api.get('clientes/clientes/');
     return res.data;
 };
+
+// Crear un nuevo cliente
 export const crearCliente = async (data) => {
     const res = await api.post('clientes/clientes/', data);
     return res.data;
 };
+
+// Actualizar cliente existente
 export const actualizarCliente = async (id, data) => {
     const res = await api.put(`clientes/clientes/${id}/`, data);
     return res.data;
 };
+
+// Eliminar cliente
 export const eliminarCliente = async (id) => {
     await api.delete(`clientes/clientes/${id}/`);
 };
