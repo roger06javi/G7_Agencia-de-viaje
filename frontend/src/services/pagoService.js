d4e327f64ee98318421110bd53b650b84e22ab87
Camila Tuso <camila.tuso9119@utc.edu.ec>
2026-08-03 22:03:49 -0500

    Agregar comentarios al servicio de pagos

--- frontend/src/services/pagoService.js
+++ frontend/src/services/pagoService.js
@@ -1,17 +1,24 @@
 import api from './api';
 
+// Obtener todos los pagos
 export const obtenerPagos = async () => {
     const res = await api.get('pagos/');
     return res.data;
 };
+
+// Crear un nuevo pago
 export const crearPago = async (data) => {
     const res = await api.post('pagos/', data);
     return res.data;
 };
+
+// Actualizar pago existente
 export const actualizarPago = async (id, data) => {
     const res = await api.put(`pagos/${id}/`, data);
     return res.data;
 };
+
+// Eliminar pago
 export const eliminarPago = async (id) => {
     await api.delete(`pagos/${id}/`);
 };
