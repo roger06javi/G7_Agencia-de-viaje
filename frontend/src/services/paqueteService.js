3e7a519510c71dd455b3664d3c9da8f0eea3fe4b
Camila Tuso <camila.tuso9119@utc.edu.ec>
2026-08-03 22:00:43 -0500

    Agregar comentarios al servicio de paquetes

--- frontend/src/services/paqueteService.js
+++ frontend/src/services/paqueteService.js
@@ -1,17 +1,24 @@
 import api from './api';
 
+// Obtener todos los paquetes
 export const obtenerPaquetes = async () => {
     const res = await api.get('paquetes/');
     return res.data;
 };
+
+// Crear un nuevo paquete
 export const crearPaquete = async (data) => {
     const res = await api.post('paquetes/', data);
     return res.data;
 };
+
+// Actualizar paquete existente
 export const actualizarPaquete = async (id, data) => {
     const res = await api.put(`paquetes/${id}/`, data);
     return res.data;
 };
+
+// Eliminar paquete
 export const eliminarPaquete = async (id) => {
     await api.delete(`paquetes/${id}/`);
 };
