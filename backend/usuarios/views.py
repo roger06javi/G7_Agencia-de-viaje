0adacea27c975d45c9fe3f9c8ceac7cf5b9d7631
Royce <roger.ugsha9244@utc.edu.ec>
2026-08-04 09:33:19 -0500

    Crear la views.py para el modelo de usuario.

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,3 +1,4 @@
+# Crear la vista para el modelo de usuario, con permisos de autenticación y métodos para listar, crear, actualizar y eliminar usuarios.
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
